eb63e9d63cbd56312527c532646835abba6a554a
Bartłomiej Kaczmarski <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-24 19:46:26 +0100

    Zadanie Wykonane(Tylko napisanie funkcji)

--- ObsługaWyjątków/Program.cs
+++ ObsługaWyjątków/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿public static double TrianglePerimeter(int a, int b, int c, int precision = 2)
+{
+    if (precision <= 1 || precision >= 9)
+    {
+        throw new ArgumentOutOfRangeException("wrong arguments");
+
+    }
+    if (a < 0 || b < 0 || c < 0)
+    {
+        throw new ArgumentOutOfRangeException("wrong arguments");
+    }
+
+    if (a + b < c || a + c < b || c + b < a)
+    {
+        throw new ArgumentException("object not exists");
+    }
+    double obwod = a + b + c;
+    return obwod;
+}
